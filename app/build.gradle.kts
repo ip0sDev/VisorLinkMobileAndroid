68edfbf8ad2ff593331ce48081985ba0268b1027
IposDev <ale1523.11@gmail.com>
2026-08-24 19:54:31 +0300

    -Обновлен AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "by.iposdev.visorlink"
         minSdk = 30
         targetSdk = 37
-        versionCode = 128
-        versionName = "3.3.03"
+        versionCode = 129
+        versionName = "3.3.04"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("long", "BUILD_TIMESTAMP", "${System.currentTimeMillis()}L")
         buildConfigField("String", "CHANNEL", "\"CANARY\"")
