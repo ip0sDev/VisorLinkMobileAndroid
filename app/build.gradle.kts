c18e4905f090aeb497f10e2647df22d7d2786db1
IposDev <57806689+ip0sDev@users.noreply.github.com>
2026-09-03 00:22:59 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "by.iposdev.visorlink"
         minSdk = 30
         targetSdk = 37
-        versionCode = 136
-        versionName = "3.5.01"
+        versionCode = 137
+        versionName = "3.5.02"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("long", "BUILD_TIMESTAMP", "${System.currentTimeMillis()}L")
         buildConfigField("String", "CHANNEL", "\"BETA\"")
