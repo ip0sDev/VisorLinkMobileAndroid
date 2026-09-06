c73dd2559fb0a1e83b07074fca44c7bc071ec06b
IposDev <57806689+ip0sDev@users.noreply.github.com>
2026-09-06 16:20:56 +0300

    -Исправления в работе самообновления

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "by.iposdev.visorlink"
         minSdk = 30
         targetSdk = 37
-        versionCode = 139
-        versionName = "3.5.04"
+        versionCode = 140
+        versionName = "3.5.05"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("long", "BUILD_TIMESTAMP", "${System.currentTimeMillis()}L")
         buildConfigField("String", "CHANNEL", "\"BETA\"")
